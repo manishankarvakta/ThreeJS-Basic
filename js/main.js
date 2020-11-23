cfbb9e48af01cc95047556526d2d14acc53386fd
Manishankar Vakta <evilrider555@gmail.com>
2020-11-23 13:26:32 +0600

    lights

--- js/main.js
+++ js/main.js
@@ -25,11 +25,11 @@
         // create shape
         var geometry = new THREE.BoxGeometry(1,1,1);
         var cubeMaterials = [
-            new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/1.png'), side: THREE.DoubleSide}), //Right Side
-            new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/2.png'), side: THREE.DoubleSide}), // Left side
-            new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/3.png'), side: THREE.DoubleSide}), // top side
+            new THREE.MeshLambertMaterial({map: new THREE.TextureLoader().load('img/1.png'), side: THREE.DoubleSide}), //Right Side
+            new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load('img/2.png'), side: THREE.DoubleSide}), // Left side
+            new THREE.MeshLambertMaterial({map: new THREE.TextureLoader().load('img/3.png'), side: THREE.DoubleSide}), // top side
             new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/4.png'), side: THREE.DoubleSide}), // bottom side
-            new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/5.png'), side: THREE.DoubleSide}), // front side
+            new THREE.MeshLambertMaterial({map: new THREE.TextureLoader().load('img/5.png'), side: THREE.DoubleSide}), // front side
             new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/6.png'), side: THREE.DoubleSide}), // back side
         ];
         
@@ -41,10 +41,42 @@
 
         camera.position.z = 3;
 
+        // LIGHT
+        // ambienetLight
+        var ambientLight = new THREE.AmbientLight(0xffffff, 1);
+        scene.add(ambientLight);
+
+        // PointLight1
+        var pointLight1 = new THREE.PointLight(0xff0040, 1.1, 50);
+        scene.add(pointLight1);
+
+        // PointLight
+        var pointLight2 = new THREE.PointLight(0x0040ff, 1.2, 50);
+        scene.add(pointLight2);
+
+        // PointLight
+        var pointLight3 = new THREE.PointLight(0x80ff80, 1.005, 50);
+        scene.add(pointLight3);
+
+
+
         // update [game lagic]
         var update = function(){
             cube.rotation.x += 0.005;
             cube.rotation.y += 0.005;
+
+            var time = Date.now()*00005;
+            pointLight1.position.x = Math.sin(time * 0.7)*30;
+            pointLight1.position.y = Math.cos(time * 0.5)*40;
+            pointLight1.position.z = Math.cos(time * 0.3)*30;
+
+            pointLight2.position.x = Math.sin(time * 0.7)*30;
+            pointLight2.position.y = Math.cos(time * 0.5)*40;
+            pointLight2.position.z = Math.sin(time * 0.3)*30;
+
+            pointLight3.position.x = Math.sin(time * 0.7)*30;
+            pointLight3.position.y = Math.sin(time * 0.5)*40;
+            pointLight3.position.z = Math.cos(time * 0.3)*30;
         };
 
         // render
